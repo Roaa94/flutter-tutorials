36d05266aa3ae9ad9fb7fc761791e48f851f10c7
roaa94 <roaa94khaddam@gmail.com>
2021-12-12 10:04:20 +0300

    extracted avatar uploader widget

--- lib/file-upload-service-tutorial/ui/file_upload_service_home_page.dart
+++ lib/file-upload-service-tutorial/ui/file_upload_service_home_page.dart
@@ -4,29 +4,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tutorials/file-upload-service-tutorial/data/services/file/file_uploader_service.dart';
 import 'package:flutter_tutorials/file-upload-service-tutorial/data/services/service_locator.dart';
+import 'package:flutter_tutorials/file-upload-service-tutorial/ui/widgets/avatar_uploader.dart';
 
-class FileUploadServiceHomePage extends StatefulWidget {
+class FileUploadServiceHomePage extends StatelessWidget {
   static const String routeName = 'file-upload-service-home-page';
-
-  @override
-  State<FileUploadServiceHomePage> createState() => _FileUploadServiceHomePageState();
-}
-
-class _FileUploadServiceHomePageState extends State<FileUploadServiceHomePage> {
-  final FileUploaderService _fileUploaderService = getIt<FileUploaderService>();
-
-  File? imageFile;
-  bool _isLoadingGettingImage = false;
-
-  Future _getImage() async {
-    setState(() => _isLoadingGettingImage = true);
-    final pickedImageFile = await _fileUploaderService.uploadImage(context);
-    setState(() => _isLoadingGettingImage = false);
-
-    if (pickedImageFile != null) {
-      setState(() => imageFile = pickedImageFile);
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,47 +17,7 @@ class _FileUploadServiceHomePageState extends State<FileUploadServiceHomePage> {
         title: const Text('File Upload Service'),
       ),
       body: Center(
-        child: GestureDetector(
-          onTap: _isLoadingGettingImage ? null : _getImage,
-          child: Container(
-            width: 200,
-            height: 200,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.blueGrey.withOpacity(0.5),
-              border: Border.all(width: 3, color: Colors.teal),
-              image: imageFile == null
-                  ? null
-                  : DecorationImage(
-                      image: FileImage(imageFile!),
-                      fit: BoxFit.cover,
-                    ),
-            ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(100),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  const SizedBox(height: 70),
-                  if (_isLoadingGettingImage) const CircularProgressIndicator(),
-                  const SizedBox(height: 30),
-                  Container(
-                    height: 50,
-                    width: 200,
-                    color: Colors.black.withOpacity(0.5),
-                    padding: const EdgeInsets.only(bottom: 12),
-                    child: Center(
-                      child: Text(
-                        imageFile == null ? 'Upload Image' : 'Change Image',
-                        style: const TextStyle(color: Colors.white, fontSize: 16),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
+        child: AvatarUploader(),
       ),
     );
   }
---
+++ lib/file-upload-service-tutorial/ui/widgets/avatar_uploader.dart
@@ -0,0 +1,72 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_tutorials/file-upload-service-tutorial/data/services/file/file_uploader_service.dart';
+import 'package:flutter_tutorials/file-upload-service-tutorial/data/services/service_locator.dart';
+
+class AvatarUploader extends StatefulWidget {
+  @override
+  _AvatarUploaderState createState() => _AvatarUploaderState();
+}
+
+class _AvatarUploaderState extends State<AvatarUploader> {
+  final FileUploaderService _fileUploaderService = getIt<FileUploaderService>();
+
+  File? imageFile;
+  bool _isLoadingGettingImage = false;
+
+  Future _getImage() async {
+    setState(() => _isLoadingGettingImage = true);
+    final pickedImageFile = await _fileUploaderService.uploadImage(context);
+    setState(() => _isLoadingGettingImage = false);
+
+    if (pickedImageFile != null) {
+      setState(() => imageFile = pickedImageFile);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _isLoadingGettingImage ? null : _getImage,
+      child: Container(
+        width: 200,
+        height: 200,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.blueGrey.withOpacity(0.5),
+          border: Border.all(width: 3, color: Colors.teal),
+          image: imageFile == null
+              ? null
+              : DecorationImage(
+                  image: FileImage(imageFile!),
+                  fit: BoxFit.cover,
+                ),
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(100),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              const SizedBox(height: 70),
+              if (_isLoadingGettingImage) const CircularProgressIndicator(),
+              const SizedBox(height: 30),
+              Container(
+                height: 50,
+                width: 200,
+                color: Colors.black.withOpacity(0.5),
+                padding: const EdgeInsets.only(bottom: 12),
+                child: Center(
+                  child: Text(
+                    imageFile == null ? 'Upload Image' : 'Change Image',
+                    style: const TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
