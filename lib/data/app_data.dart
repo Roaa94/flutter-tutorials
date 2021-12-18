f17652a7def18a6d0e315972946a8afb2042f1ca
roaa94 <roaa94khaddam@gmail.com>
2021-12-18 13:59:51 +0300

    modified first tutorial data in app_data.dart

--- lib/data/app_data.dart
+++ lib/data/app_data.dart
@@ -1,11 +1,16 @@
+import 'package:flutter_tutorials/avatar-uploader-tutorial/ui/file_upload_service_home_page.dart';
+import 'package:flutter_tutorials/refresh-indicator-thread/refresh_indicator_thread_home.dart';
+
 import 'models/tutorial.dart';
 
 class AppData {
-  static List<Tutorial> tutorials = [
+  static List<Tutorial> appTutorials = [
     Tutorial(
       title: 'Image Uploader With App Permissions and Compression Using GetIt Services',
       description:
           'Allow image upload in your Flutter app with one line of code in your widget while handling everything from camera & gallery permissions to image compression under the hood with GetIt and the service locator pattern.',
+      tutorialPageRoute: FileUploadServiceHomePage.routeName,
+      tutorialLink: 'https://medium.com/@roaa_k/flutter-image-uploader-with-app-permissions-and-compression-using-getit-services-59ffea13f913',
     ),
   ];
 }
