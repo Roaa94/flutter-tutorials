70d88cfbffc7920c88e9dc9991940f47d5db799f
roaa94 <roaa94khaddam@gmail.com>
2021-12-19 23:55:18 +0300

    optimized main_theme_switcher.dart

--- lib/mains/main_theme_switcher.dart
+++ lib/mains/main_theme_switcher.dart
@@ -216,16 +216,29 @@ class AppColors {
     return _primaryColors;
   }
 
+  static Color getShade(Color color, {bool darker = false, double value = .1}) {
+    assert(value >= 0 && value <= 1);
+
+    final hsl = HSLColor.fromColor(color);
+    final hslDark = hsl.withLightness((darker ? (hsl.lightness - value) : (hsl.lightness + value)).clamp(0.0, 1.0));
+
+    return hslDark.toColor();
+  }
+
   static MaterialColor getMaterialColorFromColor(Color color) {
-    String colorHex = color.value.toRadixString(16).substring(2);
-    List<int> _shades = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
-    Map<int, Color> _colorShades = {};
-    for (int i = 0; i < _shades.length; i++) {
-      String _opacityHex = (255 / _shades.length + ((255 / _shades.length) * i)).ceil().toRadixString(16);
-      Color _colorShade = Color(int.parse('0x$_opacityHex$colorHex'));
-      _colorShades[_shades[i]] = _colorShade;
-    }
-    return MaterialColor(int.parse('0xff$colorHex'), _colorShades);
+    Map<int, Color> _colorShades = {
+      50: getShade(color, value: 0.5),
+      100: getShade(color, value: 0.4),
+      200: getShade(color, value: 0.3),
+      300: getShade(color, value: 0.2),
+      400: getShade(color, value: 0.1),
+      500: color,
+      600: getShade(color, value: 0.1, darker: true),
+      700: getShade(color, value: 0.15, darker: true),
+      800: getShade(color, value: 0.2, darker: true),
+      900: getShade(color, value: 0.25, darker: true),
+    };
+    return MaterialColor(color.value, _colorShades);
   }
 }
 
